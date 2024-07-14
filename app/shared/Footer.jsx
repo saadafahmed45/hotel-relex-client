250e16fb65e869388db83a152eb26f4c0e28076c
Mohammad Haolder <73881636+saadafahmed45@users.noreply.github.com>
2024-07-14 11:59:32 +0600

    Update Footer.jsx

--- app/shared/Footer.jsx
+++ app/shared/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <footer className="px-24 divide-y dark:bg-gray-100 border-t-2 dark:text-gray-800">
+    <footer className="px-4 lg:px-24 divide-y dark:bg-gray-100 border-t-2 dark:text-gray-800">
       <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
         <div className="lg:w-1/3">
           <a
