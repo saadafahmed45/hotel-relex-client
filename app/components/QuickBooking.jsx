2c0bfdc1bfc91acd136bbb815644366488f11f7d
yousufislamme <online.okk@gmail.com>
2024-07-15 00:50:00 +0600

    quck booking added

--- app/components/QuickBooking.jsx
+++ app/components/QuickBooking.jsx
@@ -1,21 +1,51 @@
+ 
 
 const QuickBooking = () => {
+  
   return (
-    
-    <div className="">
-      <div className="bg-white/60 rounded-sm backdrop-blur-lg mx-20 py-10">
-      <div className='select-none grid grid-cols-6  px-5'>
-        <div className="">box</div>
-        <div className="">box</div>
-        <div className="">box</div>
-        <div className="">box</div>
-        <div className="">box</div>
-        <div className="">box</div>
-   
+    <div className="bg-white/60 rounded-sm backdrop-blur-sm mx-20 py-5">
+      <div className="select-none px-5">
+        <table className="block w-full">
+            <tr className="grid grid-cols-6 ">
+              {/* input for check in or out */}
+            <td className="border-[1px] border-black p-2">
+              <input className="bg-transparent" type="date" name="check-in" /></td>
+            <td className="border-[1px] border-black p-2">
+              <input type="date" className="bg-transparent" name="check-out" />
+              </td>
+              {/* select option */}
+              <td className="border-[1px] border-black w-full p-2">
+              <select id="adult" className="bg-transparent">
+                <option value="1 Adjust">1 Adult</option>
+                <option value="2 Adjust">2 Adult</option>
+                <option value="3Adjust">3 Adult</option>
+                <option value="saab">4 Adult</option>
+              </select>
+              </td>
+            <td className="border-[1px] border-black p-2">
+                 <select className="bg-transparent">
+                    <option value="1 Children">1 Children</option>
+                    <option value="2 Adjust">2 Children</option>
+                    <option value="c">3 Children</option>
+                    <option value="Children4">4 Children</option>
+                  </select>
+              </td>
+            <td className="border-[1px] border-black p-2">
+                          <select className="bg-transparent">
+                    <option value="1 Room">1 Room</option>
+                    <option value="2 room">2 Room</option>
+                    <option value="3 room">3 Room</option>
+                    <option value="Children4">4 Children</option>
+                  </select>
+              </td>
+            <td className="border-[1px] bg-white  border-black p-2">
+              <button className="block w-full h-full">check now</button>
+              </td>
+            </tr>
+        </table>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default QuickBooking;
