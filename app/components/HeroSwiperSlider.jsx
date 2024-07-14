7eb0b3bc11a0d778f5b69442021ae5a7bbcf7d04
Md Yousuf <71249987+yousufislamme@users.noreply.github.com>
2024-07-14 14:29:46 +0600

    Update HeroSwiperSlider.jsx

--- app/components/HeroSwiperSlider.jsx
+++ app/components/HeroSwiperSlider.jsx
@@ -6,36 +6,39 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import QuickBooking from './QuickBooking';
 
-
-
 const HeroSwiperSlider = () => {
- 
   return (
-     <div className='flex flex-col'>
-        <section className='relative'>
-           
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-           <SwiperSlide>
-              <div className=''>
-                     <img className='object-cover select-none'
-                        src="https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                        
-                     />
-              </div>
-        </SwiperSlide>
-           <SwiperSlide>
-              <div className=' '>
-                 <img src="https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="img" />
-              </div></SwiperSlide>
-           <SwiperSlide>
-             <div className=''>
-                 <img src="https://images.pexels.com/photos/210265/pexels-photo-210265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-              </div></SwiperSlide>
-      </Swiper>
-        </section>
-     <div className="absolute left-0 bottom-[300px] lg:bottom-0 right-0  z-50 w-full">
+    <div className='flex flex-col'>
+      <section className='relative'>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>
+            <div className=''>
+              <img className='object-cover select-none w-full h-screen'
+                src="https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className=''>
+              <img className='object-cover select-none w-full h-screen'
+                src="https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+                alt="img" 
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className=''>
+              <img className='object-cover select-none w-full h-screen'
+                src="https://images.pexels.com/photos/210265/pexels-photo-210265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+                alt="" 
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </section>
+      <div className="absolute left-0 bottom-0 right-0 z-50 w-full">
         <QuickBooking />
-     </div>
+      </div>
     </div>
   )
 }
