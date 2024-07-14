58c86d624b70cbb0188dd47a8c562ef8d3b3853e
Md Yousuf <71249987+yousufislamme@users.noreply.github.com>
2024-07-14 12:58:29 +0600

    Update HeroSwiperSlider.jsx

--- app/components/HeroSwiperSlider.jsx
+++ app/components/HeroSwiperSlider.jsx
@@ -33,7 +33,7 @@ const HeroSwiperSlider = () => {
               </div></SwiperSlide>
       </Swiper>
         </section>
-     <div className="absolute left-0 bottom-0 right-0  z-50 w-full">
+     <div className="absolute left-0 bottom-[300px] lg:bottom-0 right-0  z-50 w-full">
         <QuickBooking />
      </div>
     </div>
