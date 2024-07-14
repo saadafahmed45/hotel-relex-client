d43d68c884a8c19b6fa01eb1e3a7d6da98d23636
Md Yousuf <71249987+yousufislamme@users.noreply.github.com>
2024-07-14 17:33:23 +0600

    bug fixing for mobile #7 CustomerState.jsx

--- app/components/CustomerState.jsx
+++ app/components/CustomerState.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { bookingApi } from "../api/api";
 
-const CustomerState = async () => {
-  const bookings = await bookingApi();
-  //   console.log(bookings.length);
+const CustomerState = () => {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      const result = await bookingApi();
+      setBookings(result);
+    };
+
+    fetchBookings();
+  }, []);
+
   return (
     <section className="p-6 my-6 bg-gray-100 text-gray-800">
-      <div className="container grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 xl:grid-cols-4">
+      <div className="container grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
           <div className="flex justify-center p-2 align-middle rounded-lg sm:p-4 bg-violet-600">
             <svg
