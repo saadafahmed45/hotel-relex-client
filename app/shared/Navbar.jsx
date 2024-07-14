6d1b4f646462c33ebb946a3e1b74e7b018832b31
Md Yousuf <71249987+yousufislamme@users.noreply.github.com>
2024-07-14 17:14:26 +0600

    Update Mobile Navbar.jsx

--- app/shared/Navbar.jsx
+++ app/shared/Navbar.jsx
@@ -3,20 +3,22 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
-
 const Navbar = () => {
-    const pathname = usePathname();
-    const [isVisible, setIsVisible] = useState(true);
+  const pathname = usePathname();
+  const [isVisible, setIsVisible] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
-   const dropdownRef = useRef(null);
-    const handleScroll = () => {
-        if (window.scrollY > lastScrollY) {
-          setIsVisible(false); // Scroll down
-        } else {
-          setIsVisible(true); // Scroll up
-        }
-        setLastScrollY(window.scrollY);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+      setIsVisible(false); // Scroll down
+    } else {
+      setIsVisible(true); // Scroll up
+    }
+    setLastScrollY(window.scrollY);
   };
+
   const handleDropdownToggle = () => {
     setIsDropdownVisible((prev) => !prev);
   };
@@ -26,7 +28,8 @@ const Navbar = () => {
       setIsDropdownVisible(false);
     }
   };
-    useEffect(() => {
+
+  useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     document.addEventListener("mousedown", handleClickOutside);
 
@@ -35,11 +38,9 @@ const Navbar = () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [lastScrollY]);
-  
+
   return (
-    <header className={`first-letter: fixed top-0 z-50 w-full border-b-2 bg-white/50 backdrop-blur-2xl px-1 transition-transform duration-300 md:px-5 lg:px-20 ${
-          !isVisible ? "-translate-y-full" : "translate-y-0"
-        }`}>
+    <header className={`fixed top-0 z-50 w-full border-b-2 bg-white/50 backdrop-blur-2xl px-1 transition-transform duration-300 md:px-5 lg:px-20 ${!isVisible ? "-translate-y-full" : "translate-y-0"}`}>
       <div className="container flex justify-between items-center h-16 mx-auto">
         <Link
           rel="noopener noreferrer"
@@ -47,28 +48,14 @@ const Navbar = () => {
           aria-label="Back to homepage"
           className="flex items-center p-2 font-semibold text-2xl"
         >
-          Hotel
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 32 32"
-            className="w-8 h-8 dark:text-violet-600"
-          >
-            <path d="M27.912 7.289l-10.324-5.961c-0.455-0.268-1.002-0.425-1.588-0.425s-1.133 0.158-1.604 0.433l0.015-0.008-10.324 5.961c-0.955 0.561-1.586 1.582-1.588 2.75v11.922c0.002 1.168 0.635 2.189 1.574 2.742l0.016 0.008 10.322 5.961c0.455 0.267 1.004 0.425 1.59 0.425 0.584 0 1.131-0.158 1.602-0.433l-0.014 0.008 10.322-5.961c0.955-0.561 1.586-1.582 1.588-2.75v-11.922c-0.002-1.168-0.633-2.189-1.573-2.742zM27.383 21.961c0 0.389-0.211 0.73-0.526 0.914l-0.004 0.002-10.324 5.961c-0.152 0.088-0.334 0.142-0.53 0.142s-0.377-0.053-0.535-0.145l0.005 0.002-10.324-5.961c-0.319-0.186-0.529-0.527-0.529-0.916v-11.922c0-0.389 0.211-0.73 0.526-0.914l0.004-0.002 10.324-5.961c0.152-0.090 0.334-0.143 0.53-0.143s0.377 0.053 0.535 0.144l-0.006-0.002 10.324 5.961c0.319 0.185 0.529 0.527 0.529 0.916z"></path>
-            <path d="M22.094 19.451h-0.758c-0.188 0-0.363 0.049-0.515 0.135l0.006-0.004-4.574 2.512-5.282-3.049v-6.082l5.282-3.051 4.576 2.504c0.146 0.082 0.323 0.131 0.508 0.131h0.758c0.293 0 0.529-0.239 0.529-0.531v-0.716c0-0.2-0.11-0.373-0.271-0.463l-0.004-0.002-5.078-2.777c-0.293-0.164-0.645-0.26-1.015-0.26-0.39 0-0.756 0.106-1.070 0.289l0.010-0.006-5.281 3.049c-0.636 0.375-1.056 1.055-1.059 1.834v6.082c0 0.779 0.422 1.461 1.049 1.828l0.009 0.006 5.281 3.049c0.305 0.178 0.67 0.284 1.061 0.284 0.373 0 0.723-0.098 1.027-0.265l-0.012 0.006 5.080-2.787c0.166-0.091 0.276-0.265 0.276-0.465v-0.716c0-0.293-0.238-0.529-0.529-0.529z"></path>
-          </svg>
-          Relex
+          Hotel Relex
         </Link>
         <ul className="hidden space-x-3 md:flex">
           <li className="">
             <Link
               rel="noopener noreferrer"
               href={"/"}
-              className={`flex items-center px-4 dark:border- dark:text-violet-600 dark:border-violet-600 ${
-                pathname === "/"
-                  ? "text-violet-600"
-                  : ""
-              }`}
+              className={`flex items-center px-4 ${pathname === "/" ? "text-violet-600" : ""}`}
             >
               Home
             </Link>
@@ -77,25 +64,16 @@ const Navbar = () => {
             <Link
               rel="noopener noreferrer"
               href={"/about"}
-              className={`flex items-center px-4 dark:border- dark:text-violet-600 dark:border-violet-600 ${
-                pathname === "/about"
-                  ? "text-violet-600"
-                  : ""
-              }`}
+              className={`flex items-center px-4 ${pathname === "/about" ? "text-violet-600" : ""}`}
             >
               About
             </Link>
           </li>
-
           <li className="flex">
             <Link
               rel="noopener noreferrer"
               href={"/rooms"}
-              className={`flex items-center px-4 dark:border- dark:text-violet-600 dark:border-violet-600 ${
-                pathname === "/rooms"
-                  ? "text-violet-600"
-                  : ""
-              }`}
+              className={`flex items-center px-4 ${pathname === "/rooms" ? "text-violet-600" : ""}`}
             >
               Room
             </Link>
@@ -104,11 +82,7 @@ const Navbar = () => {
             <Link
               rel="noopener noreferrer"
               href={"/login"}
-              className={`flex items-center px-4 dark:border- dark:text-violet-600 dark:border-violet-600 ${
-                pathname === "/login"
-                  ? "text-violet-600"
-                  : ""
-              }`}
+              className={`flex items-center px-4 ${pathname === "/login" ? "text-violet-600" : ""}`}
             >
               Login
             </Link>
@@ -117,21 +91,13 @@ const Navbar = () => {
             <Link
               rel="noopener noreferrer"
               href={"/dashboard"}
-              className={`flex items-center px-4 dark:border- dark:text-violet-600 dark:border-violet-600 ${
-                pathname === "/dashboard"
-                  ? "text-violet-600"
-                  : ""
-              } ${
-                pathname === "/dashboard"
-                  ? "text-violet-600"
-                  : ""
-              }`}
+              className={`flex items-center px-4 ${pathname === "/dashboard" ? "text-violet-600" : ""}`}
             >
               Dashboard
             </Link>
           </li>
         </ul>
-        <button className="flex justify-end p-4 md:hidden">
+        <button className="flex justify-end p-4 md:hidden" onClick={handleDropdownToggle}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -148,6 +114,62 @@ const Navbar = () => {
           </svg>
         </button>
       </div>
+      {isDropdownVisible && (
+        <div ref={dropdownRef} className="md:hidden absolute top-16 left-0 w-full bg-white border-t">
+          <ul className="flex flex-col space-y-2 p-4">
+            <li>
+              <Link
+                rel="noopener noreferrer"
+                href={"/"}
+                className={`block px-4 py-2 ${pathname === "/" ? "text-violet-600" : ""}`}
+                onClick={() => setIsDropdownVisible(false)}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                rel="noopener noreferrer"
+                href={"/about"}
+                className={`block px-4 py-2 ${pathname === "/about" ? "text-violet-600" : ""}`}
+                onClick={() => setIsDropdownVisible(false)}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                rel="noopener noreferrer"
+                href={"/rooms"}
+                className={`block px-4 py-2 ${pathname === "/rooms" ? "text-violet-600" : ""}`}
+                onClick={() => setIsDropdownVisible(false)}
+              >
+                Room
+              </Link>
+            </li>
+            <li>
+              <Link
+                rel="noopener noreferrer"
+                href={"/login"}
+                className={`block px-4 py-2 ${pathname === "/login" ? "text-violet-600" : ""}`}
+                onClick={() => setIsDropdownVisible(false)}
+              >
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link
+                rel="noopener noreferrer"
+                href={"/dashboard"}
+                className={`block px-4 py-2 ${pathname === "/dashboard" ? "text-violet-600" : ""}`}
+                onClick={() => setIsDropdownVisible(false)}
+              >
+                Dashboard
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 };
