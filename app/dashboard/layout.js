8712a1bd4ba85515ca508e1c86cc99ba24fc6708
Md Yousuf <71249987+yousufislamme@users.noreply.github.com>
2024-07-14 17:39:16 +0600

    Update layout.js

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,16 +1,16 @@
 import Sidebar from "../components/Sidebar";
 
-export default function DashboardLayout({
-  children, // will be a page or nested layout
-}) {
+export default function DashboardLayout({ children }) {
   return (
-    <section className="px-8 py-16 flex">
+    <section className="flex flex-col md:flex-row px-4 md:px-8 py-8 md:py-16">
       {/* Include shared UI here e.g. a header or sidebar */}
-      <div className="w-[20%] ">
+      <div className="w-full md:w-[20%] mb-8 md:mb-0">
         <Sidebar />
       </div>
 
-      <div className="p-4 w-screen ">{children}</div>
+      <div className="w-full md:w-[80%] p-4">
+        {children}
+      </div>
     </section>
   );
 }
