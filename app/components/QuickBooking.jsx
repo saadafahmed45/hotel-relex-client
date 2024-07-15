17c3afb7e320ac9157dc0bed78eb9471fe947b13
yousufislamme <online.okk@gmail.com>
2024-07-15 22:47:01 +0600

    simple bug fixing in QuickBooking

--- app/components/QuickBooking.jsx
+++ app/components/QuickBooking.jsx
@@ -9,21 +9,21 @@ const QuickBooking = () => {
             <tr className="grid grid-cols-6 ">
               {/* input for check in or out */}
             <td className="border-[1px] border-black p-2">
-              <input className="bg-transparent" type="date" name="check-in" /></td>
+              <input className="bg-transparent w-full"  type="date" name="check-in" /></td>
             <td className="border-[1px] border-black p-2">
-              <input type="date" className="bg-transparent" name="check-out" />
+              <input type="date" className="bg-transparent w-full" name="check-out" />
               </td>
               {/* select option */}
               <td className="border-[1px] border-black w-full p-2">
-              <select id="adult" className="bg-transparent">
-                <option value="1 Adjust">1 Adult</option>
-                <option value="2 Adjust">2 Adult</option>
-                <option value="3Adjust">3 Adult</option>
-                <option value="saab">4 Adult</option>
+              <select id="adult" className="bg-transparent w-full">
+                <option className="w-full" value="1 Adjust">1 Adult</option>
+                <option className="w-full" value="2 Adjust">2 Adult</option>
+                <option className="w-full" value="3Adjust">3 Adult</option>
+                <option className="w-full" value="saab">4 Adult</option>
               </select>
               </td>
             <td className="border-[1px] border-black p-2">
-                 <select className="bg-transparent">
+                 <select className="bg-transparent w-full">
                     <option value="1 Children">1 Children</option>
                     <option value="2 Adjust">2 Children</option>
                     <option value="c">3 Children</option>
@@ -31,7 +31,7 @@ const QuickBooking = () => {
                   </select>
               </td>
             <td className="border-[1px] border-black p-2">
-                          <select className="bg-transparent">
+                          <select className="bg-transparent w-full h-full">
                     <option value="1 Room">1 Room</option>
                     <option value="2 room">2 Room</option>
                     <option value="3 room">3 Room</option>
