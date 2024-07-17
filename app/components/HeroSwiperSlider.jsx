65e10ae832df29bb4a8b373461ca96588c8e1646
yousufislamme <online.okk@gmail.com>
2024-07-18 00:26:47 +0600

    add motion effect

--- app/components/HeroSwiperSlider.jsx
+++ app/components/HeroSwiperSlider.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
+import { motion } from "framer-motion";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
@@ -16,7 +17,18 @@ const HeroSwiperSlider = () => {
           className="mySwiper !h-[600px]"
         >
           <SwiperSlide>
-            <div>
+            <div className="relative">
+              <div className="absolute bottom-[470px] left-0 right-0">
+                <motion.h1
+                  whileInView={{ y: 0, opacity: 1 }}
+                  initial={{ y: 100, opacity: 0 }}
+                  viewport={{ once: true }}
+                  transition={{ duration: 1, stiffness: 100 }}
+                  className="text-3xl"
+                >
+                  Hello World
+                </motion.h1>
+              </div>
               <img
                 className="h-screen w-full select-none object-cover"
                 src="https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
